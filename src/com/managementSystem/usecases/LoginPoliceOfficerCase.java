4091723b43634529eb67d730fd7b08c45b63beb0
sank29 <wankhedes29@gamil.com>
2022-10-02 17:09:17 +0530

    handle some exception and added project database discription text file

--- src/com/managementSystem/usecases/LoginPoliceOfficerCase.java
+++ src/com/managementSystem/usecases/LoginPoliceOfficerCase.java
@@ -1,10 +1,13 @@
 package com.managementSystem.usecases;
 
+import java.sql.SQLException;
+
 import com.managementSystem.dao.CrimeImp;
+import com.managementSystem.exception.MySqlExceptions;
 
 public class LoginPoliceOfficerCase {
 	
-	public  boolean loginPoliceOfficer(String username, String password,String officerName, int officerId) {
+	public  boolean loginPoliceOfficer(String username, String password,String officerName, int officerId) throws MySqlExceptions, SQLException {
 		CrimeImp crimeImp = new CrimeImp();
 		
 		if(crimeImp.loginPolicOfficer(username, password,officerName,officerId) == null)
